7aa8bfc60fb159eb5ab596db9356c641869bf113
fiopwk <flopwiki@gmail.com>
2020-01-30 20:59:42 -0500

    updated navbar directory name

--- src/components/students/navBar/MainNavbar.js
+++ src/components/students/navBar/MainNavbar.js
@@ -1,43 +1,30 @@
 import React, {useState} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
 import './Navbar.scss';
 
 function MainNavbar(props) {
 
 
-    return (
-        <Navbar className='header' style={{background: 'rgba(0,0,0,.5)', color: '#FFFFFF'}}>
-            <Navbar.Brand href="#home" style={{color: '#FFF'}}>LOGO HERE</Navbar.Brand>
-            <Nav className="mr-auto">
-                <p className='one-day-a-line'>International School Social Worker</p>
-            </Nav>
-            <Link exact to="/">
-
-                <Nav>
-                    Home
-                </Nav>
-            </Link>
-            <Link to="/add-students">
-
-                <Nav>
-                    Add Students
-                </Nav>
-            </Link>
-            <Link>
-
-                <Nav>
-                    Modify Students
-                </Nav>
-            </Link>
-            <Link to="">
-
-                <Nav>
-                    Logout
-                </Nav>
-            </Link>
-        </Navbar>
-    );
+   return(
+    <Navbar className='header' style={{background: 'rgba(0,0,0,.5)', color: '#FFFFFF'}}>     
+      <Navbar.Brand href="#home" style={{color:'#FFF'}}>LOGO HERE</Navbar.Brand>      
+      <Nav className="mr-auto">
+        <p className='one-day-a-line'>International School Social Worker</p>       
+      </Nav> 
+      <Nav>         
+        Home
+      </Nav>
+      <Nav>         
+        Add Students
+      </Nav>
+      <Nav>         
+         Modify Students
+      </Nav>
+      <Nav>         
+         Logout
+      </Nav>    
+    </Navbar>
+   );
 }
 
 export default MainNavbar;
