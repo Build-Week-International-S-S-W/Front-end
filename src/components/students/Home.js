42d41ed32a91d24720f1981f9d6063af232bc245
fiopwk <flopwiki@gmail.com>
2020-02-01 18:25:01 -0500

    Home: added button to homepage

--- src/components/students/Home.js
+++ src/components/students/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
 
 export default function Home() {
    return(
      <div>
       <h1>Educell International School</h1>
+      <div>
+        <Button onClick={(e) => console.log('2', 2 + e)}>Add Student</Button>
+      </div>
      </div>
    );
 }
