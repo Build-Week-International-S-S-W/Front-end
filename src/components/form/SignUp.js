6b46e715b35f124dd9838920a602f0b4810f026b
vy3191 <venkyafhs@gmail.com>
2020-01-31 21:24:21 -0500

    started working on sign form using formik

--- src/components/form/SignUp.js
+++ src/components/form/SignUp.js
@@ -1,10 +1,48 @@
 import React, { useState, useEffect} from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 export default function SignUp(){
+   const defaultUser = {username:'', email:'', phoneNumber:'', password:'' };
+   const [user, setUser] = useState(defaultUser);
+   const handleSubmit = (values,formikBag) => {
+       console.log(values);
+       console.log(formikBag);
+       formikBag.resetForm();
+   }
    return(
-     <div>
-       <h3>Sign-Up form</h3>
-     </div>
-   )
+    <React.Fragment>  
+    <Formik       
+       initialValues = {user}
+       onSubmit = {handleSubmit}    
+      render={(props) => {
+        const {isSubmitting} = props;
+        console.log(isSubmitting);
+        return(
+          <Form>
+            <Field type="text" 
+                    name="username"
+                    placeholder="Enter your name"                
+            />
+            <Field type="text" 
+                    name="email"
+                    placeholder="Enter your email"                
+            />
+            <Field type="text" 
+                    name="phoneNumber"
+                    placeholder="phone number"                
+            />
+            <Field type="password" 
+                    name="password"
+                    placeholder="Password"                
+            />
+            { isSubmitting ? <button type="submit">Submitting...</button> :
+                <input type="submit" />}
+          </Form>
+        )
+      }}
+     />
+    </React.Fragment>
+   );
 
-}
+};
