11f8c32c9fa71bcdae824d191b3100c3c08d7b83
Jeff Glanville <jeff.glanville@outlook.com>
2020-02-01 19:42:09 -0600

    add map to home

--- src/components/students/Home.js
+++ src/components/students/Home.js
@@ -2,18 +2,29 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 export default function Home() {
-  const [student, setStudent] = useState('');
+  const [student, setStudent] = useState([{name: ""}]);
 
   useEffect(() => {
     axios
       .get('https://international-school-sw.herokuapp.com/api/students')
-      .then((res) => setStudent(res.data.name))
+      .then((res) => {
+        console.log(res)
+        setStudent(res.Results)})
       .catch((err) => console.log(err))
-  })
+  }, [])
 
    return(
-     <div>
-      <h1>Educell International School</h1>
-     </div>
+
+     <>
+      {/* <h1>Educell International School</h1> */}
+
+      {student.map((person) => {
+        console.log(person)
+        // return (
+        //   // <p>{person.name}</p>
+        // )
+      })}
+
+     </>
    );
 }
