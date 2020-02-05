c0f30fef2fd40279ad7414801761099b5ea3f716
vy3191 <venkyafhs@gmail.com>
2020-02-04 20:46:43 -0500

    Added action types and imported in reducers

--- src/reducers/students.js
+++ src/reducers/students.js
@@ -1,4 +1,30 @@
-import { STUDENTS_GETTING_START, 
+import {
+        STUDENTS_GETTING_START, 
+        STUDENTS_GETTING_SUCCESS, 
+        STUDENTS_GETTING_ERROR,
+        STUDENTS_ADDING_START,
+        STUDENTS_ADDING__SUCCESS,
+        STUDENTS_ADDING__ERROR,
+        STUDENTS_UPDATE_START,
+        STUDENTS_UPDATE_SUCCESS,
+        STUDENTS_UPDATE_ERROR,
+        STUDENTS_DELETE_START,
+        STUDENTS_DELETE_SUCCESS,
+        STUDENTS_DELETE_ERROR } from '../actions/students';
 
-
-        }
+const initialState = {
+        isLoading:false,
+        students:[
+              {
+                name:'',
+                student_grade:'',
+                student_class:'',
+                background:'',
+                student_status:'',
+                age:'',
+                special_needs:'',
+                insurance:false,
+                birth_certificate:false
+              }
+            ]
+};
