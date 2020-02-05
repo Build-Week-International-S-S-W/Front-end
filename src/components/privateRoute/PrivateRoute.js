d4e44c0aab27be5d7adde1b3691011c60bd3ce89
vy3191 <venkyafhs@gmail.com>
2020-02-04 20:19:32 -0500

    Added privateRoute feature to the application

--- src/components/privateRoute/PrivateRoute.js
+++ src/components/privateRoute/PrivateRoute.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import {SESSION_KEY_TOKEN } from '../../'
+import {SESSION_KEY_TOKEN } from '../../constants/constatnt';
 
-export default function PrivateRoute({component:Component, {...rest}}) {
+export default function PrivateRoute({component:Component, ...rest}) {
+   const token = sessionStorage.getItem(SESSION_KEY_TOKEN);
    return <Route {...rest} render={(props) => {
-          if()
+          if(token) {
+             return <Component {...props} />
+          } else {
+             return <Redirect to="/login" />
+          }
    }}/>
 }
