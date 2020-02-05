478ba0d4431260e72a9cdd07e70450adcc6c4e3a
fiopwk <flopwiki@gmail.com>
2020-02-05 18:49:03 -0500

    setup Edit form

--- src/components/administration/EditForm.js
+++ src/components/administration/EditForm.js
@@ -1,109 +1,39 @@
-import React, {useState} from 'react'
-import {Formik, Form, Field} from 'formik';
+import React, {useState, useEffect} from 'react';  
+import { useForm } from 'react-hook-form'; 
+import {useHistory, useParams} from 'react-router-dom';
+import { server } from '../../axios';
 
-function EditForm() {
-    const [editStudent] = useState({
+export default function EditForm(props) {
+    const { studentsList } = props;
+    const { handleSubmit, errors} = useForm();
+    const history = useHistory();
+    const {student_id} = useParams;
+    const [students, editStudents] = useState({});
 
-        studentName: '',
-        age: '',
-        grade: '',
-        story: '',
-        insurance: '',
-        needs: '',
-        expiration: '',
-        birthCertificate: '',
-    });
+    useEffect(() => {
+        const student = studentsList.find((student) => {
+            return student._id === student_id;
+        });
+        editStudents(student)
+    },[studentsList, student_id]);
 
-    const [editRepresentative] = useState({
-        repFirstName: '',
-        repLastName: '',
-        phone: '',
-        email: '',
-        relation: '',
+    const onSubmit = (data) => {
+        const student = data;
+        console.log(student)
+        server.put(`/students/${student_id}`, student)
+        .then(res => console.log(res))
+        .then(history.push('/administration'))
+        .catch(err => console.log(err))
+    };
 
-    });
+    if (!students) { return <h1>Loading...</h1>}
 
     return (
-        <div className="edit-form">
-            <Formik>
-                initialValues=({editStudent, editRepresentative})
+        <div className="container">
+            <h1>Edit Student</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-                <Form>
-                    <div className="edit-student-info">
-                        Student Information
-                        <label
-                            htmlFor="studentName">Name:
-                            <Field type="name" name="studentName" placeholder="Student Name">
-                            </Field>
-                        </label>
-
-                        <label htmlFor="grade">Grade:</label>
-                            <Field as="select" name="grade" placeholder="Grade">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                            </Field>
-                    </div>
-                    <div className="edit-back-story">
-                        <label
-                            htmlFor="story">Backstory:
-                        </label>
-                        <Field type="textarea" name="story" placeholder="Student Backstory"/>
-                    </div>
-                    <div>
-                        <label htmlFor="Y">: Yes</label>
-                        <Field type="checkbox" name="Y"/>
-
-                        <label htmlFor="N">: No</label>
-                        <Field type="checkbox" name="N"/>
-
-                        <label htmlFor="expiration">Expiration Date:</label>
-                        <Field type="date" name="expiration"/>
-                        <br/>
-
-                        <label htmlFor>Birth Certificate</label>
-
-                        <label htmlFor="y">: Yes</label>
-                        <Field type="checkbox" name="y"/>
-
-                        <label htmlFor="n">: No</label>
-                        <Field type="checkbox" name="n"/>
-
-                        <label htmlFor="needs">Special Needs:</label>
-                        <Field type="textarea" name="needs" placeholder="Allergies"/>
-                    </div>
-                    <br/>
-
-                    <div className="edit-rep-info">
-                        Representative Information
-
-                    <label htmlFor="repName">Name:</label>
-                    <Field type="name" name="repName" placeholder="Name"/>
-
-                    <label htmlFor="phone">Phone:</label>
-                    <Field type="phone" name="phone" placeholder="optional"/>
-
-                    <label htmlFor="email">Email:</label>
-                    <Field type="email" name="email" placeholder="Required"/>
-                    </div>
-
-                    <label htmlFor="relation">Contact:</label>
-                    <Field type="textarea" name="relation" placeholder="Contact"/>
-                </Form>
-            </Formik>
-
+            </form>
         </div>
     )
 }
-
-
-export default EditForm;
