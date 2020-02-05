5fe5af2a3cabd56084381d967894df914417a64e
vy3191 <venkyafhs@gmail.com>
2020-02-04 21:41:47 -0500

    store is wiredup with BioPage.js

--- src/components/students/aboutStudents/BioPage.js
+++ src/components/students/aboutStudents/BioPage.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getStudents } from '../../../actions/students';
 import axios from 'axios';
 
 import {
@@ -10,124 +12,120 @@ import {
     CarouselCaption
 } from 'reactstrap';
 
-const AboutStudents = () => {
+const AboutStudents = (props) => {
+    
     const [activeIndex, setActiveIndex] = useState(0);
     const [animating, setAnimating] = useState(false);
-    const [students, setStudents] = useState([]);
 
     useEffect(() => {
-        axios.get('https://international-school-sw.herokuapp.com/api/students')
-            .then(res => {
-                setStudents(res.data)
-                // console.log(res.data)
-            }
-            ).catch(err => console.log(err))
+        props.getStudents();       
     }, []);
 
+    // const next = () => {
+    //     if (animating) return;
+    //     const nextIndex = activeIndex === students.length - 1 ? 0 : activeIndex + 1;
+    //     setActiveIndex(nextIndex);
+    // };
 
+    // const previous = () => {
+    //     if (animating) return;
+    //     const nextIndex = activeIndex === 0 ? students.length - 1 : activeIndex - 1;
+    //     setActiveIndex(nextIndex);
+    // };
 
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === students.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    };
+    // const goToIndex = (newIndex) => {
+    //     if (animating) return;
+    //     setActiveIndex(newIndex);
+    // };
 
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? students.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    };
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    };
-
-    const classmates = students.map((student) => {
-
-
-        return (
-            <CarouselItem
-                className="custom-tag"
-                tag="div"
-                key={student.id}
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(false)}
-                unmountOnExit={() => setAnimating(true)}
-            >
-                <CarouselCaption
-
-                    captionText={student.name}
-
-                />
-
-
-
-            </CarouselItem>
-
-
-
-        );
-
-    });
-
+    // const classmates = students.map((student) => {
+    //    console.log(students);
+    //     return (
+    //         <CarouselItem
+    //             className="custom-tag"
+    //             tag="div"
+    //             key={student.id}
+    //             onExiting={() => setAnimating(true)}
+    //             onExited={() => setAnimating(false)}
+    //             unmountOnExit={() => setAnimating(true)}
+    //         >
+    //             <CarouselCaption
+    //                 captionText={student.name}
+    //             />
+    //         </CarouselItem>
+    //     );
+    // });
+    console.log(props.studentsList);
     return (
-
-        <div>
-             <style>
-                {
-                    `.custom-tag {
-              max-width: 50%;
-
-              height: 300px;
-
-              background: black;
-              font-size: 29px;
-
-            }`
-                }
-            </style>
-
-            <Carousel
-                activeIndex={activeIndex}
-                next={next}
-                previous={previous}
-
-            >
-                <CarouselIndicators
-                    items={students}
-                    students={students}
-                    activeIndex={activeIndex}
-                    onClickHandler={goToIndex}
-                />
-
-                {classmates}
-                <CarouselControl
-                    direction="prev"
-                    directionText="Previous"
-                    onClickHandler={previous}
-
-                />
-                <CarouselControl
-                    direction="next"
-                    directionText="Next"
-                    onClickHandler={next}
-                />
-
-            </Carousel >
-
+        <React.Fragment>
+            {props.studentsList.map( (student) => {
+                return <div>
+                    <h1>{student.name}</h1>
+                </div>
+            })}
             <div className="btn">
                 <Button color="success" onClick={(touched) => {
-                    console.log(classmates.map( fixed => fixed.valueOf()));
+                    // console.log(classmates.map( fixed => fixed.valueOf()));
                     touched = (alert('Educell International network is being monitor!'));
 
                     window.location.href = "/add-students"}}>Add Student</Button>
             </div>
-
-
-
-        </div>
+        </React.Fragment>          
     )
 };
 
-export default AboutStudents;
+const mapStateToProps = (state) => {
+    console.log(state.students)
+    return{
+        error:state.students.error,
+        isLoading:state.students.isLoading,
+        studentsList:state.students.students
+    }
+}
+
+export default connect( mapStateToProps, { getStudents })(AboutStudents);
+
+
+
+   
+        //      <style>        
+        //         {
+        //             `.custom-tag {
+        //       max-width: 50%;
+
+        //       height: 300px;
+
+        //       background: black;
+        //       font-size: 29px;
+
+        //     }`
+        //         }
+        //     </style>
+
+        //     <Carousel
+        //         activeIndex={activeIndex}
+        //         next={next}
+        //         previous={previous}
+
+        //     >
+        //         <CarouselIndicators
+        //             items={students}
+        //             students={students}
+        //             activeIndex={activeIndex}
+        //             onClickHandler={goToIndex}
+        //         />
+
+        //         {classmates}
+        //         <CarouselControl
+        //             direction="prev"
+        //             directionText="Previous"
+        //             onClickHandler={previous}
+
+        //         />
+        //         <CarouselControl
+        //             direction="next"
+        //             directionText="Next"
+        //             onClickHandler={next}
+        //         />
+
+        //     </Carousel >
