972b56024fed495be61429e5293b1ae248602f69
vy3191 <venkyafhs@gmail.com>
2020-02-04 20:57:49 -0500

    written getStudetns action creator

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 export const STUDENTS_GETTING_START = 'STUDENTS_GETTING_START';
 export const STUDENTS_GETTING_SUCCESS = 'STUDENTS_GETTING_START';
 export const STUDENTS_GETTING_ERROR = 'STUDENTS_GETTING_START';
@@ -13,3 +14,16 @@ export const STUDENTS_UPDATE_ERROR = 'STUDENTS_UPDATE_START';
 export const STUDENTS_DELETE_START = 'STUDENTS_DELETE_START';
 export const STUDENTS_DELETE_SUCCESS = 'STUDENTS_DELETE_START';
 export const STUDENTS_DELETE_ERROR = 'STUDENTS_DELETE_START';
+
+export function getStudents() {
+   return (dispatch) => {
+      dispatch({type:STUDENTS_GETTING_START});
+      axios.get('https://international-school-sw.herokuapp.com/api/students')
+           .then( res => {
+               console.log(res);
+           })
+           .catch(err => {
+
+           });
+   }
+}
