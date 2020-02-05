4a3f5d4206e615718e3a8ddc7a838458d7475f02
fiopwk <flopwiki@gmail.com>
2020-02-05 07:41:30 -0500

    fixed Add Students

--- src/components/students/addStudents/IntakeForm.js
+++ src/components/students/addStudents/IntakeForm.js
@@ -3,193 +3,187 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Button, Alert } from 'reactstrap';
 import './intakestyles.scss'
 import '../../assests/css/_schoolConfig.scss';
+import moment from 'moment';
 
 
 
-export default function IntakeForm(props) {
+function IntakeForm(props) {
+    console.log('props =>', props);
+    const { name, age, student_grade, student_class, background, insurance, student_needs, expiration, birth_certificate, student_status } = props;
     const [student, setStudent] = useState({
 
-        studentName: '',
+        name: '',
         age: '',
-        grade: '',
-        story: '',
+        student_grade: '',
+        student_class: '',
+        background: '',
         insurance: '',
-        needs: '',
+        special_needs: '',
         expiration: '',
-        birthCertificate: '',
+        birth_certificate: '',
+        student_status: '',
 
     });
 
     const [representative, setRepresentative] = useState({
-        repFirstName: '',
-        repLastName: '',
-        phone: '',
+        name: '',
+        phone_number: '',
         email: '',
         relation: '',
 
     });
+    const lastVisit = () => {
+        moment().startOf('day').fromNow();
+    }
 
     return (
-        <>
+        <React.Fragment>
+            <Formik
+                initialValues={{ student, representative }}
+                onSubmit={(values, formikBag, lastVisit) => {setStudent(); formikBag.resetForm(); lastVisit.props.push(setStudent)}}
+                validate={values => {
+                    const errors = {};
 
-            <div className="Intake-Form">
-                {
-                    window.addEventListener('load', ev => {
-                        console.log(Date.now())
-                    })
-                }
+                    //We need to handle the checkboxes state inside Boolean
+                    //expression. If Y is true, then N cannot be also true,
+                    //and if n is true, then y cannot be also true.
 
-                <Formik
-                    initialValues={{ student, representative }}
-                    validate={values => {
-                        const errors = {};
-                        if (!values.email) {
-                            errors.email = 'Required';
-                        } else if (!values.studentName) {
-                            errors.studentName = 'Required';
+                    //essentially we only accept one value for each slice
+                    //of state.
+                    // else if (values.Y || values.y === true) {
+                    //     errors.n = 'Disabled';
+                    //     errors.N = false;
+                    // }
+                    if (!values.email) { errors.email = 'Email is Required' }
+                    if (!values.name) { errors.name = 'Student Name is Required' }
 
-                        } else if (values.age > 16 || !values.age ) {
-                            errors.age = 'Not Allowed';
-                        }
-                        //We need to handle the checkboxes state inside Boolean
-                        //expression. If Y is true, then N cannot be also true,
-                        //and if n is true, then y cannot be also true.
+                    return errors;
+                }}
 
-                        //essentially we only accept one value for each slice
-                        //of state.
-                        // else if (values.Y || values.y === true) {
-                        //     errors.n = 'Disabled';
-                        //     errors.N = false;
-                        // }
+                render={(props) => {
+                    const { isSubmitting } = props;
+                    return (
+                            <div className="Intake-Form">
+                        
+                            <Form className="intake">
+                                <div className='student-info'>
+                                    <Alert color="primary" className='student-info'>Student Information</Alert>
+                                    <div className='name-age-grade'>
 
-                        return errors;
-                    }}
+                                        <label htmlFor="name">Name:</label>
 
-                    onSubmit={(values, { isSubmitting }) => {
-                        console.log(values)
+                                        <Field type='name' name='name' placeholder='Required' />
 
-                    }}
-                >
-                    {({ isSubmitting }) => (
+                                        <label htmlFor="age">Age:</label>
+                                        <Field type='text' name='age' placeholder='Student age' />
+                                        <ErrorMessage name='age' component='div' />
 
-                        <Form>
-                            <div className='student-info'>
-                                <Alert color="primary" className='student-info'>Student Information</Alert>
-                                <div className='name-age-grade'>
-
-                                    <label htmlFor="studentName">Name:</label>
-
-                                    <Field type='name' name='studentName' placeholder='Required' />
-
-                                    <label htmlFor="age">Age:</label>
-                                    <Field type='text' name='age' placeholder='Student age' />
-                                    <ErrorMessage name='age' component='div'/>
-
-                                    <label htmlFor="grade">Grade:</label>
-                                    <Field as='select' name='grade'>
-                                        {
-                                            // TODO: add loop to fill in options for more DRY code
+                                        <label htmlFor="student_class">Grade:</label>
+                                        <Field as='select' name='student_class'>
+                                            {
+                                                // TODO: add loop to fill in options for more DRY code
 
 
-                                        }
-                                        {
+                                            }
 
-                                        }
 
-                                        <option value="1">1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>
-                                        <option>9</option>
-                                        <option>10</option>
-                                        <option>11</option>
-                                        <option>12</option>
+                                            <option value="1">1</option>
+                                            <option>2</option>
+                                            <option>4</option>
+                                            <option>3</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                            <option>7</option>
+                                            <option>8</option>
+                                            <option>9</option>
+                                            <option>10</option>
+                                            <option>11</option>
+                                            <option>12</option>
 
-                                    </Field>
+                                        </Field>
+                                    </div>
+
+                                    <div className="back-story">
+                                        <label htmlFor="background">BackStory:</label>
+                                        <Field type='textarea' name='background' placeholder='(Student Backstory)' />
+                                    </div>
+                                    <div className="student-bottom">
+                                        <label htmlFor="insurance">Insurance </label>
+
+
+                                        <label htmlFor="Y">:  Yes</label>
+                                        <Field type='checkbox' name='Y' />
+
+
+                                        <label htmlFor="N"> No</label>
+                                        <Field type='checkbox' name='N' />
+                                        {/*<ErrorMessage name='N' component='div'/>*/}
+
+
+                                        <label htmlFor="date">Expiration Date:</label>
+                                        <Field type='date' name='date' />
+                                        <br>
+
+                                        </br>
+
+                                        <label htmlFor="birth_certificate">Birth Certificate</label>
+
+                                        <label htmlFor="y">: Yes</label>
+                                        <Field type='checkbox' name='y' />
+
+                                        <label htmlFor="n">No</label>
+                                        <Field type='checkbox' name='n' />
+                                        {/*<ErrorMessage name='n' component='div'/>*/}
+                                        <br>
+
+                                        </br>
+
+
+                                        <label htmlFor="special_needs">Special Needs:</label>
+                                        <Field type='textarea' name='special_needs' placeholder="Allergies" />
+                                    </div>
+
+
+                                </div>
+                                <br />
+
+                                <div className='rep-info'>
+                                    <Alert color="warning" className=''>Representative Information</Alert>
+
+
+                                    <label htmlFor="repName">Name:</label>
+                                    <Field type='name' name='repName' placeholder='Name' />
+
+
+                                    <label htmlFor="phone_number">Phone:</label>
+                                    <Field type='phone' name='phone_number' placeholder='optional' />
+
+                                    <label htmlFor="email">Email:</label>
+                                    <Field type='email' name='email' placeholder='Required' />
+                                    <ErrorMessage name='email' component='div' />
+
+
+                                    <label htmlFor="relation">Contact:</label>
+                                    <Field type='textarea' name='relation' placeholder='Contact' />
                                 </div>
 
-                                <div className="back-story">
-                                    <label htmlFor="story">BackStory:</label>
-                                    <Field type='textarea' name='story' placeholder='(Student Backstory)' />
-                                </div>
-                                <div className="student-bottom">
-                                    <label htmlFor="insurance">Insurance </label>
+                                <Button onChange={(touched) => { touched ? isSubmitting = true : console.log(ErrorMessage) }} color='warning' type="submit" disabled={isSubmitting}>
+                                    Submit
+                            </Button>
+                            </Form>
+                        </div>
+                    )
+                }}
+            />    
+        </React.Fragment>
+    );
 
+};
 
-                                    <label htmlFor="Y">:  Yes</label>
-                                    <Field type='checkbox' name='Y' />
-
-
-                                    <label htmlFor="N"> No</label>
-                                    {/*<ErrorMessage name='N' component='div'/>*/}
-                                    <Field type='checkbox' name='N' />
-
-
-                                    <label htmlFor="date">Expiration Date:</label>
-                                    <Field type='date' name='date' />
-                                    <br>
-
-                                    </br>
-
-                                    <label htmlFor="birthCertificate">Birth Certificate</label>
-
-                                    <label htmlFor="y">: Yes</label>
-                                    <Field type='checkbox' name='y' />
-
-                                    <label htmlFor="n">No</label>
-                                    {/*<ErrorMessage name='n' component='div'/>*/}
-                                    <Field type='checkbox' name='n' />
-                                    <br>
-
-                                    </br>
-
-
-                                    <label htmlFor="needs">Special Needs:</label>
-                                    <Field type='textarea' name='needs' placeholder="Allergies" />
-                                </div>
-
-
-                            </div>
-                            <br/>
-
-                            <div className='rep-info'>
-                                <Alert color="warning" className=''>Representative Information</Alert>
-
-
-                                <label htmlFor="repName">Name:</label>
-                                <Field type='name' name='repName' placeholder='Name' />
-
-
-                                <label htmlFor="phone">Phone:</label>
-                                <Field type='phone' name='phone' placeholder='optional' />
-
-                                <label htmlFor="email">Email:</label>
-                                <Field type='email' name='email' placeholder='Required' />
-                                <ErrorMessage name='email' component='div' />
-
-
-                                <label htmlFor="relation">Contact:</label>
-                                <Field type='textarea' name='relation' placeholder='Contact' />
-                            </div>
-
-                            <Button onChange={(touched) => { touched ? isSubmitting = true : console.log(ErrorMessage) }} color='warning' type="submit" enabled={isSubmitting}>
-                                Submit
-                        </Button>
-
-                        </Form>
-
-                    )}
-
-                </Formik>
-
-            </div>
-
-        </>
-    )
-
-
+const mapStateToProps = (state) => {
+    return {
+        
+    }
 }
+ export default IntakeForm;
+    
