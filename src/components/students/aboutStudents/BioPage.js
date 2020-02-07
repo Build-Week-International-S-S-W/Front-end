bb862e0b8168269878926ed2663cf1055416e145
fiopwk <flopwiki@gmail.com>
2020-02-07 18:44:09 -0500

    removed unused code from BioPage

--- src/components/students/aboutStudents/BioPage.js
+++ src/components/students/aboutStudents/BioPage.js
@@ -1,80 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getStudents } from '../../../actions/students';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
-// import { StudentsDataTable } from './DataTable';
-import axios from 'axios';
 
-import {
-    Button,
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption
-} from 'reactstrap';
+
+import {Button} from 'reactstrap';
 
 const AboutStudents = (props) => {
-    
-    // const [activeIndex, setActiveIndex] = useState(0);
-    // const [animating, setAnimating] = useState(false);
-    // const [ students, setStudents] = useState(props.studentsList);
 
     useEffect(() => {
         props.getStudents();       
-    }, []);
+    }, [props]);
 
-    // useEffect(() => {
-    //     setStudents(props.studentsList)       
-    // }, [props.studentsList]);
-
-    // const next = () => {
-    //     if (animating) return;
-    //     const nextIndex = activeIndex === students.length - 1 ? 0 : activeIndex + 1;
-    //     setActiveIndex(nextIndex);
-    // };
-
-    // const previous = () => {
-    //     if (animating) return;
-    //     const nextIndex = activeIndex === 0 ? students.length - 1 : activeIndex - 1;
-    //     setActiveIndex(nextIndex);
-    // };
-
-    // const goToIndex = (newIndex) => {
-    //     if (animating) return;
-    //     setActiveIndex(newIndex);
-    // };
-
-    // const classmates = students.map((student) => {
-    //    console.log(students);
-    //     return (
-    //         <CarouselItem
-    //             className="custom-tag"
-    //             tag="div"
-    //             key={student.id}
-    //             onExiting={() => setAnimating(true)}
-    //             onExited={() => setAnimating(false)}
-    //             unmountOnExit={() => setAnimating(true)}
-    //         >
-    //             <CarouselCaption
-    //                 captionText={student.name}
-    //             />
-    //         </CarouselItem>
-    //     );
-    // });
-    console.log('studetns infor from store>>>>>>>>>>>>>>>>',props.studentsList);
+    
+    console.log('studetns infor from store>>>>>>>>>>>>>>>>', props.studentsList);
     return (
         <React.Fragment>
-            {/* {props.studentsList.map( (student) => {
-                return <div>
-                    <h1>{student.name}</h1>
-                </div>
-            })} */}
-            {/* <StudentsDataTable data={props.studentsList}/> */}
+            
             <div className="btn">
                 <Button color="success" onClick={(touched) => {
-                    // console.log(classmates.map( fixed => fixed.valueOf()));
+                    
                     touched = (alert('Educell International network is being monitor!'));
 
                     window.location.href = "/add-students"}}>Add Student</Button>
