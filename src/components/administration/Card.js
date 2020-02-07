4f106b1b5250113795a9287e8b7438c2594f057a
Jeff Glanville <jeff.glanville@outlook.com>
2020-02-06 19:52:49 -0600

    edit cards page

--- src/components/administration/Card.js
+++ src/components/administration/Card.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Card} from 'primereact/card';
 import {Button} from 'primereact/button';
+import './Card.scss';
+
 
 export default class StudentCard extends Component {
     constructor(props) {
@@ -11,11 +13,12 @@ export default class StudentCard extends Component {
     }
     render() {
         const header = (
-            <img alt="Card" src='showcase/resources/demo/images/usercard.png'/>
+            <img alt="Card" src='https://via.placeholder.com/50'/>
         );
         const footer = (
-            <span>
+            <span id="btn">
                 <Button label="Edit" icon="pi pi-check" className="edit"/>
+
                 <Button label="Delete" icon="pi pi-times" className="delete"/>
             </span>
         );
@@ -23,24 +26,26 @@ export default class StudentCard extends Component {
         return (
             <div>
                 <div className="card-wrapper">
-                    <Card title= {this.state.student.name }
-                          subTitle = {this.state.student.id}
-                          style={{width: '360px'}}
-                          footer={footer}
-                          header={header}>
-                        <div className="card-info">
-                            <p>Grade: {this.state.student.grade}</p>
-                            <p>Class: {this.state.student.class}</p>
-                            <p>Background Story: {this.state.student.background}</p>
-                            <p>Status: {this.state.student.status}</p>
-                            <p>Age: {this.state.student.age}</p>
-                            <p>Has Insurance: {this.state.student.insurance}</p>
-                            <p>Has Birth Certificate: {this.state.student.birth_certificate}</p>
-                            <p>Special Needs: {this.state.student.special_needs}</p>
-                            <p>Student's Contact Person: {this.state.student.student_contact}</p>
-                            <p>Student's Social Worker: {this.state.student.social_worker}</p>
-                        </div>
-                    </Card>
+                    <div className="card-container">
+                        <Card title= {this.state.student.name }
+                            subTitle = {this.state.student.id}
+                            // style={{width: '360px'}}
+                            footer={footer}
+                            header={header}>
+                            <div className="card-info">
+                                <p>Grade: {this.state.student.grade}</p>
+                                <p>Class: {this.state.student.class}</p>
+                                <p>Background Story: {this.state.student.background}</p>
+                                <p>Status: {this.state.student.status}</p>
+                                <p>Age: {this.state.student.age}</p>
+                                <p>Has Insurance: {this.state.student.insurance}</p>
+                                <p>Has Birth Certificate: {this.state.student.birth_certificate}</p>
+                                <p>Special Needs: {this.state.student.special_needs}</p>
+                                <p>Student's Contact Person: {this.state.student.student_contact}</p>
+                                <p>Student's Social Worker: {this.state.student.social_worker}</p>
+                            </div>
+                        </Card>
+                    </div>
                 </div>
             </div>
         )
