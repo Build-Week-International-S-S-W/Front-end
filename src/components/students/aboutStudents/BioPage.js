3b4249a573e2f01d214c20f2857d3caffc078444
vy3191 <venkyafhs@gmail.com>
2020-02-07 21:46:17 -0500

    Fixed home page default form styles

--- src/components/students/aboutStudents/BioPage.js
+++ src/components/students/aboutStudents/BioPage.js
@@ -2,87 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getStudents } from '../../../actions/students';
 import {DataTable} from 'primereact/datatable';
-import {Button} from 'primereact/button';
+// import {Button} from 'primereact/button';
+import {Button} from 'reactstrap';
 import {Column} from 'primereact/column';
-// import { StudentsDataTable } from './DataTable';
-import axios from 'axios';
+import {Redirect} from 'react-router-dom';
+import './biostyles.scss'
 
-import {
-    // Button,
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption
-} from 'reactstrap';
 
-const AboutStudents = (props) => {
-    
-    // const [activeIndex, setActiveIndex] = useState(0);
-    // const [animating, setAnimating] = useState(false);
-    // const [ students, setStudents] = useState(props.studentsList);
+const AboutStudents = (props) => {        
     const paginatorLeft = <Button icon="pi pi-refresh"/>;
     const paginatorRight = <Button icon="pi pi-cloud-upload"/>;
-
-
     useEffect(() => {
         props.getStudents();       
     }, []);
 
-    // useEffect(() => {
-    //     setStudents(props.studentsList)       
-    // }, [props.studentsList]);
+    const addStudentsHandler = () => {
+        return <Redirect to="/add-students" />        
+    }
 
-    // const next = () => {
-    //     if (animating) return;
-    //     const nextIndex = activeIndex === students.length - 1 ? 0 : activeIndex + 1;
-    //     setActiveIndex(nextIndex);
-    // };
-
-    // const previous = () => {
-    //     if (animating) return;
-    //     const nextIndex = activeIndex === 0 ? students.length - 1 : activeIndex - 1;
-    //     setActiveIndex(nextIndex);
-    // };
-
-    // const goToIndex = (newIndex) => {
-    //     if (animating) return;
-    //     setActiveIndex(newIndex);
-    // };
-
-    // const classmates = students.map((student) => {
-    //    console.log(students);
-    //     return (
-    //         <CarouselItem
-    //             className="custom-tag"
-    //             tag="div"
-    //             key={student.id}
-    //             onExiting={() => setAnimating(true)}
-    //             onExited={() => setAnimating(false)}
-    //             unmountOnExit={() => setAnimating(true)}
-    //         >
-    //             <CarouselCaption
-    //                 captionText={student.name}
-    //             />
-    //         </CarouselItem>
-    //     );
-    // });
-    console.log('studetns infor from store>>>>>>>>>>>>>>>>',props.studentsList);
     return (
         <React.Fragment>
-            {/* {props.studentsList.map( (student) => {
-                return <div>
-                    <h1>{student.name}</h1>
-                </div>
-            })} */}
-            {/* <StudentsDataTable data={props.studentsList}/> */}
-            <div className="btn">
-                <Button color="success" onClick={(touched) => {
-                    // console.log(classmates.map( fixed => fixed.valueOf()));
-                    touched = (alert('Educell International network is being monitor!'));
-
-                    window.location.href = "/add-students"}}>Add Student</Button>
-            </div>
+            <Button onClick={() => addStudentsHandler}>Add Student</Button>        
             <DataTable value={props.studentsList}
                        sortMode="multiple"
                        paginator={true} 
